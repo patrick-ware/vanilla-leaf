72790612aaf6bc1d45eb4117e312bf8c2256c2c7
Patrick Ware <patrick.r.ware@gmail.com>
2021-03-27 12:52:31 -0700

    update default map positioning

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([39.8283, -98.5795], 5);
 
 L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
