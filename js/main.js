b1144168e3ea3cdbfe2080f4607a9b6fc6d82e73
Patrick Ware <patrick.r.ware@gmail.com>
2021-03-27 12:42:07 -0700

    openstreetmap base used

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-let mymap = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
+}).addTo( map );
